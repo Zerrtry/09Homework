20ca56a75b6c8d2eb6c27731fe31437317a9de81
Zerrtry <zerrtry@gmail.com>
2020-06-18 16:10:03 -0500

    functional version

--- index.js
+++ index.js
@@ -1,27 +1,47 @@
 const inquirer = require('inquirer');
 const generateMarkdown = require('./utils/generateMarkdown.js')
-const fs = require('fs');
+
 inquirer.prompt([
-    {
-       type:'input',
-       name: 'first',
-       message: 'What is your project called ?' 
-    },
-    {
-        type:'input',
-        name: 'second',
-        message: 'Please give a brief Description!' 
-     }
-  ])
-  .then(function(answers)  {
-      console.log('This is frist answer!!!', answers); 
-      var readMeString = `
-# Title ${answers.first}
-# Description ${answers.second}
-        asdfasd
-      `
-      fs.writeFile('readMe.md',readMeString , function (err) {
-        if (err) throw err;
-        console.log('Saved!');
-      });
-  })
+  {
+    type:'input',
+    name: 'Titile',
+    message: 'What is the Project title?',
+ },
+  {
+    type:'input',
+    name: 'Discription',
+    message: 'Description',
+ },
+ {
+     type:'input',
+     name: 'Installation',
+     message: 'Installation process',
+  },
+  {
+    type:'input',
+    name: 'Usage',
+    message: 'Usage',
+  },
+  {
+    type:'list',
+    name: 'Badges',
+    message: 'License',
+    choices: ['MIT', 'Creative Commons'],
+  },
+  {
+    type:'input',
+    name: 'Contributing',
+    message: 'Contributing',
+  },
+  {
+    type:'input',
+    name: 'Tests',
+    message: 'Tests',
+  }
+])
+.then(function(answers)  {
+  console.log('This is the answers!!!', answers);
+  generateMarkdown(answers)
+  
+});
+
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,8 +1,27 @@
-function generateMarkdown(data) {
-  return `
-# ${data.title}
+const fs = require('fs');
 
-`;
-}
+const generateMarkdown = (answers) => {
+  console.log('generator was called')
+    let badgeUrl = ''
+    if(answers.Badges === 'MIT') {
+      badgeUrl = '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)'
+    } else  if (answers.Badges === 'Creative Commons') {
+      badgeUrl = '[![License: CC0-1.0](https://licensebuttons.net/l/zero/1.0/80x15.png)](http://creativecommons.org/publicdomain/zero/1.0/)'
+    }
+    let readMeString = `
+    # Title ${answers.Titile};
+    # Description ${answers.Discription};
+    # Installation process ${answers.Installation};
+    # Usage ${answers.Usage};
+    ${badgeUrl};
+    # Contributing ${answers.Contributing};
+    # Tests ${answers.Tests}
+    
+    `
+    fs.writeFile('readMe.md',readMeString, (err) => {
+      if (err) throw err;
+      console.log('Saved!');
+    });
+  }
 
 module.exports = generateMarkdown;
